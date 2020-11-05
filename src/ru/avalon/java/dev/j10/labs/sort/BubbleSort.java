2b0aa96ec3020d520582a5e75075ee23d5767c70
konivex <47462070+konivex@users.noreply.github.com>
2020-11-05 19:39:28 +0300

    Initial commit

---
+++ src/ru/avalon/java/dev/j10/labs/sort/BubbleSort.java
@@ -0,0 +1,24 @@
+package ru.avalon.java.dev.j10.labs.sort;
+
+import ru.avalon.java.dev.j10.labs.Sort;
+
+/**
+ * Пузырьковая сортировка (англ. bubble sort).
+ *
+ * <p>Простой алгоритм сортировки. Для понимания и
+ * реализации этот алгоритм — простейший, но эффективен он
+ * лишь для небольших массивов. Сложность алгоритма: O(n^2).
+ *
+ * @see <a href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%BF%D1%83%D0%B7%D1%8B%D1%80%D1%8C%D0%BA%D0%BE%D0%BC">Пузырьковая сортировка</a>
+ */
+public class BubbleSort implements Sort {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void sort(int[] array) {
+        /*
+         * TODO(Студент): Реализовать метод sort класса BubbleSort
+         */
+    }
+}
---
+++ src/ru/avalon/java/dev/j10/labs/sort/SelectionSort.java
@@ -0,0 +1,25 @@
+package ru.avalon.java.dev.j10.labs.sort;
+
+import ru.avalon.java.dev.j10.labs.Sort;
+
+/**
+ * Сортировка выбором (англ. selection sort).
+ *
+ * <p>Алгоритм сортировки. Может быть как устойчивый, так и
+ * неустойчивый. На массиве из n элементов имеет время
+ * выполнения в худшем, среднем и лучшем случае Θ(n2),
+ * предполагая что сравнения делаются за постоянное время.
+ *
+ * @see <a href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%B2%D1%8B%D0%B1%D0%BE%D1%80%D0%BE%D0%BC">Сортировка выбором</a>
+ */
+public class SelectionSort implements Sort {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void sort(int[] array) {
+        /*
+         * TODO(Студент): Реализовать метод sort класса SelectionSort
+         */
+    }
+}
